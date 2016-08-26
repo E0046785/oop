6d79063ef67a666d08a2aa97116e7c4096c86815
E0046785 <E0046785@u.nus.edu>
2016-08-26 13:19:38 +0800

    Worrkshop 2 and 3

---
+++ Workshop2/Workshop2/AnotherBankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop2
+{
+    class AnotherBankAccount
+    {
+        private double balance;
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+        public void SetBalance(double balance)
+        {
+            this.balance = balance;
+        }
+    }
+}
---
+++ Workshop2/Workshop2/Coin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISS.RV.LIB;
+
+namespace Workshop2
+{
+    class Coin
+    {
+        private int face;
+
+        // Methods
+        public int GetFace()
+        {
+            return face;
+        }
+
+        public void Flip()
+        {
+            face = ISSMath.RNDInt(2);
+        }
+
+        // Constructors
+        public Coin()
+        {
+            Flip();
+        }
+
+        // Properties
+        public string StrFace
+        {
+            get
+            {
+                if (GetFace() == 0) return "HEAD";
+                else return "TAIL";
+            }
+        }
+    }
+    public class CoinApp
+    {
+        public static void Main()
+        {
+            Coin coin1 = new Coin();
+            coin1.Flip();
+            coin1.Flip(); Console.WriteLine("Coin1: {0}", coin1.StrFace);
+            coin1.Flip(); Console.WriteLine("Coin1: {0}", coin1.StrFace);
+            coin1.Flip(); Console.WriteLine("Coin1: {0}", coin1.StrFace);
+
+            Coin coin2 = new Coin();
+            coin2.Flip(); Console.WriteLine("Coin2: {0}", coin2.StrFace);
+
+            Console.WriteLine("Coin1: {0}", coin1.StrFace);
+        }
+    }       
+}
